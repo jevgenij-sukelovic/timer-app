1956161a10325986b5b0cb279fbeec1b79712e1f
jevgenij <jevgenij.sukelovic@gamil.com>
2021-10-24 21:45:35 +0300

    clearInterval added

--- Timer/script.js
+++ Timer/script.js
@@ -1,6 +1,6 @@
-const date = '2021-12-31';
+const date = '2021-10-24 21:44';
 
-const deadPoint0 = new Date(date);
+const deadPoint = new Date(date);
 
 const showDate = document.querySelector('.date');
 showDate.innerHTML = `Our dead point is: ${date}`;
@@ -20,17 +20,17 @@ const days =Math.floor(t/(1000*60*60*24)) ,
 
 
 return {
+    total: t,
     days:days,
     hours: hours,
     minutes: minutes,
     seconds: seconds
 
 };
-    
+    }
 
+   
 
-
-}
 function getZero(num){
     if(num<10 && num>0){
         return `0${num}`;
@@ -41,31 +41,44 @@ function getZero(num){
 
 
 
-function showTimer (selector){
+function showTimer (selector, endPoint){
 
     let timer  = document.querySelector(selector),
     days = timer.querySelector('#days'),
     hours = timer.querySelector('#hours'),
     minutes = timer.querySelector('#minutes'),
     seconds = timer.querySelector('#seconds');
+
     const time = setInterval(timerGo, 1000);
+
+  
+
 
     timerGo();
 
 
             function timerGo(){
-               const data =  countDateItems(deadPoint0);
+               const data =  countDateItems(endPoint);
+              
                
                     days.innerHTML = getZero( data.days);
                     hours.innerHTML = getZero(data.hours);
                     minutes.innerHTML = getZero(data.minutes);
                     seconds.innerHTML = getZero(data.seconds);
 
-                    
+                    if(data.total<=0){
+                        clearInterval(time);
+                        days.innerHTML = '0';
+                        hours.innerHTML = '0';
+                        minutes.innerHTML = '0';
+                        seconds.innerHTML = '0';
+                    }
 
             }
+            
+           
 
 
 }
-showTimer('.MyTimer');
+showTimer('.MyTimer',deadPoint);
 
